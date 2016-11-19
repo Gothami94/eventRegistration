bf7e574f194f886e11f16aba0ac42480cadd836f
Gothami94 <gothamikarunarathna@gmail.com>
2016-11-19 13:24:50 +0530

    updated

---
+++ application/controllers/Test.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function view($page = 'Test')
+	{
+		// https://www.codeigniter.com/userguide3/helpers/html_helper.html 
+		$this->load->helper('html');
+		// $this->load->helper('html'); not used
+		// http://www.formget.com/codeigniter-loading-css-and-javascript-file/
+		$this->load->helper('url');
+		//echo base_url(); // work after $this->load->helper('url');
+		// load multiple helper
+		// $this->load->helper(
+		//	array('helper1', 'helper2', 'helper3')
+		// );
+        if ( ! file_exists(APPPATH.'views/fft/'.$page.'.php'))
+        {
+                // Whoops, we don't have a page for that!
+                show_404();
+        }
+		$data['title'] = $page; // Capitalize the first letter
+        $this->load->view('fft/'.$page, $data);
+        
+	}	
+		public function index()
+	{
+		$this->load->helper('html');
+		$this->load->helper('url');
+		$this->load->view('index');
+	}
+}
+?>
